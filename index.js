e0d5631cbe72270eeac19fe6b179383f182d67a8
omer <peynirciomerfaruk@gmail.com>
2022-06-09 17:14:35 +0200

    pages are added

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const http = require("http");
+const fs = require("fs");
 const host = "127.0.0.1";
 const port = 3000;
+
+const indexPage = fs.readFileSync("index.html");
+const aboutPage = fs.readFileSync("about.html");
+const contactPage = fs.readFileSync("contact.html");
+const notFoundPage = fs.readFileSync("404.html");
 const server = http.createServer((req, res) => {
-  console.log("request url: ", req.url);
-  res.setHeader("Content-Type", "text/plain");
-  res.statusCode = 200;
-  res.end("Hi, this is my first server");
+  switch (req.url) {
+    case "/":
+      res.end(indexPage);
+      break;
+    case "/about":
+      res.end(aboutPage);
+      break;
+    case "/contact":
+      res.end(contactPage);
+      break;
+    default: {
+      
+      res.statusCode = 404;
+      res.end(notFoundPage);
+    }
+  }
 });
 server.listen(port, host, () => {
   console.log(`listening on: http://${host}:${port}`);
